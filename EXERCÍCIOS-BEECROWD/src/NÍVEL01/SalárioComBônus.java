c1c620c694ebb337930c7159b4036d08dbe20c0f
kevinCorumba <92128308+KevinCorumbaAlexandre@users.noreply.github.com>
2022-05-12 14:45:27 -0300

    modificado

--- EXERCÍCIOS-BEECROWD/src/NÍVEL01/SalárioComBônus.java
+++ EXERCÍCIOS-BEECROWD/src/NÍVEL01/SalárioComBônus.java
@@ -7,16 +7,16 @@ public class SalárioComBônus {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		System.out.println("CALCULO DE COMISSÃO DE 15%\n");
-		System.out.println("Digite o nome do funcíonario:");
+		System.out.println("CALCULO DE SALARIO + COMISSÃO DE 15%\n");
+		System.out.println("Digite o nome do funcíonario(a):");
 		String nome = sc.next();
 		System.out.println("Digite o valor do salário:");
 		double salario = sc.nextDouble();
 		System.out.println("Digite o valor das vendas :");
 		double vd = sc.nextDouble();
 		double comissao = (vd / 100) * 15 + salario;
-
-		System.out.println(String.format("TOTAL DO SALARIO+COMISSÃO : %.2f", comissao));
+		System.out.printf("Sr(a):" + nome + "\n");
+		System.out.println(String.format("\nTOTAL DO SALARIO+COMISSÃO : %.2f", comissao));
 
 	}
 
